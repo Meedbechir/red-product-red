be47dee5aaa4b8d19b2964d307da7b819b8ff53e
MeedBechir <medbechirathie@gmail.com>
2024-01-25 22:58:26 +0000

    Test last

--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const app = express();
+const APP = express();
 const cors = require('cors');
 const mongoose = require('mongoose');
 const authRoutes = require('./routes/authRoutes');
@@ -10,8 +10,8 @@ require('dotenv').config();
 
 const PORT = process.env.PORT || 5000;
 
-app.use(cors());
-app.use(express.json());
+APP.use(cors());
+APP.use(express.json());
 
 const storage = multer.diskStorage({
   destination: function (req, file, cb) {
@@ -26,26 +26,15 @@ const upload = multer({ storage: storage });
 
 
 
-app.use('/images', express.static('images'));
-app.use(upload.single('img'));
+APP.use('/images', express.static('images'));
+APP.use(upload.single('img'));
 
 const serverStartTime = new Date();
 console.log(`Le serveur a démarré le ${serverStartTime}`);
 
-if (process.env.NODE_ENV === 'production') {
-  app.use(express.static(path.join(__dirname, '/frontend/build')));
 
-  app.get('*', (req, res) => 
-  res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html'))
-  );
-}else {
-  app.get('/', (req, res) => {
-    res.send('API is running...');
-  })
-}
-
-app.use('/api', authRoutes);
-app.use('/api', hotelRoutes);
+APP.use('/api', authRoutes);
+APP.use('/api', hotelRoutes);
 
 mongoose.connect(process.env.DB_URL).then((result) => {
   console.log('Connexion à la base de données réussie !');
@@ -53,6 +42,6 @@ mongoose.connect(process.env.DB_URL).then((result) => {
   console.log(err);
 });
 
-app.listen(PORT, () => {
+APP.listen(PORT, () => {
   console.log(`Le serveur est en cours d'exécution sur le port ${PORT}`);
 });
